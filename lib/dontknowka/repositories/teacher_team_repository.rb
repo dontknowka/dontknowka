0cdf328a6338dc3ed6f02107d6735d5b10854adb
Alexander Morozov <gelu.speculum@gmail.com>
2021-03-09 16:42:43 +0300

    Fix teacher team association

--- lib/dontknowka/repositories/teacher_team_repository.rb
+++ lib/dontknowka/repositories/teacher_team_repository.rb
@@ -1,2 +1,6 @@
 class TeacherTeamRepository < Hanami::Repository
+  assignments do
+    has_many :teacher_to_teams
+    has_many :teachers, through: :teacher_to_teams
+  end
 end
--- lib/dontknowka/repositories/team_mapping_repository.rb
+++ lib/dontknowka/repositories/team_mapping_repository.rb
@@ -1,17 +1,17 @@
 class TeamMappingRepository < Hanami::Repository
   associations do
-    belongs_to :team
+    belongs_to :teacher_team
     belongs_to :homework_instance
   end
 
   def with_teams
-    aggregate(:team)
+    aggregate(:teacher_team)
       .map_to(TeamMapping)
       .to_a
   end
 
   def by_instance(id)
-    aggregate(:team)
+    aggregate(:teacher_team)
       .where(homework_instance_id: id)
       .map_to(TeamMapping)
       .one
